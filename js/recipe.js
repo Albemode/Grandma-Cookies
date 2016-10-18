fbc7d51dbc39c2218bd043bb13c63b36b663c5c3
Alberto Modesto <albe.mode.24@gmail.com>
2016-10-18 11:08:08 -0400

    Sugarland js

---
+++ js/recipe.js
@@ -0,0 +1,94 @@
+
+
+
+
+var cookie_visited = Cookies.get('cookDoubleFudge');
+var doublefudgeCounter = 0;
+//--------------------------------------------------------- Double Fudge
+var cookie_visited = Cookies.get('cookLemon');
+var lemonCounter = 0;
+//--------------------------------------------------------- Lemon
+var cookie_visited = Cookies.get('cookPeanutbutter');
+var PeanutbutterCounter = 0;
+//---------------------------------------------------------Peanutbutter
+var cookie_visited = Cookies.get('cookOatmeal');
+var oatmealCounter = 0;
+//---------------------------------------------------------- Chocolate Chip
+var cookie_visited = Cookies.get('cookChocolateChip');
+var chocolateCounter = 0;
+//---------------------------------------------------------- Sugar Coat
+var cookie_visited = Cookies.get('cookSugarCoat');
+var sugerCounter = 0;
+
+
+//doublefudge onclick function
+function doublefudgeSet() {
+  var setdoublefudge = Cookies.set('cookDoubleFudge', doublefudgeCounter);
+  doublefudgeCounter++
+    document.getElementById("DFC").innerHTML = "Double Fudge Cookies: " + doublefudgeCounter;
+}
+//lemon onclick function
+function lemonSet() {
+  var setlemon = Cookies.set('cookLemon', lemonCounter);
+  lemonCounter++
+    document.getElementById("LC").innerHTML = "Lemon Cookies: " + lemonCounter;
+}
+//peanutbutter onclick function
+function peanutbutterSet() {
+  var setpeanutbutter = Cookies.set('cookPeanutbutter', peanutbutterCounter);
+  lemonCounter++
+    document.getElementById("PBC").innerHTML = "Lemon Cookies: " + lemonCounter;
+}
+//oatmeal onclick function
+function oatmealSet() {
+  var setoatmeal = Cookies.set('cookOatmeal', oatmealCounter);
+  oatmealCounter++
+    document.getElementById("OMC").innerHTML = "Oatmeal Cookies: " + oatmealCounter;
+}
+//chocolate onclick function
+function chocolatechipSet() {
+  var setChocolate = Cookies.set('cookChocolateChip', chocolateCounter);
+  chocolateCounter++
+    document.getElementById("CCC").innerHTML = "Chocolate Chip Cookies: " + chocolateCounter;
+}
+//sugar onclick function
+function sugarcoatSet() {
+  var setSugar = Cookies.set('cookSugarCoat', sugarCounter);
+  sugarCounter++
+    document.getElementById("SC").innerHTML = "Sugar Coat Cookies: " + sugarCounter;
+        if (Cookies.get("cookSugarCoat") != null) {
+            Cookies.set("cookSugarCoat", (parseInt(Cookies.get("cookSugarCoat")) + SugarCoat));
+              $("#SC").html(Cookies.get("cookSugarCoat"));
+        } else {
+          Cookies.set("cookieSugar", sugar);
+          $("#cookSugarCoat").html(Cookies.get("cookieSugar"));
+    }
+}
+
+////remove cookie counter
+
+function myBtn() {
+  //rest Double Fudge Counter
+  Cookies('cookDoubleFudge', 0);
+      document.getElementById("DFC").innerHTML = "Double Fudge Cookies: 0";
+  
+  //reset Lemon Counter
+  Cookies('cookLemon', 0);
+      document.getElementById("LC").innerHTML = "Lemon Cookies: 0";
+  
+  //rest Peanutbutter Counter
+  Cookies('cookPeanutbutter', 0);
+      document.getElementById("PBC").innerHTML = "Peanutbutter Cookies: 0";
+  
+  //reset Oatmeal Counter
+  Cookies('cookOatmeal', 0);
+     document.getElementById("OMC").innerHTML = "Oatmeal Cookies: 0";
+  
+  //reset ChocolateChip
+  Cookies('cookChocolateChip', 0);
+      document.getElementById("CCC").innerHTML = "Chocolate Chip Cookies: 0";
+  
+  //reset SugarCoat
+  Cookies('cookSugarCoat', 0);
+      document.getElementById("SC").innerHTML = "Sugar Coat Cookies: 0";
+}
